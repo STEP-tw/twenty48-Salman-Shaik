9dac46f9183c33f5385bbead0f668ef0f48b4d0a
Salman-Shaik <dukesalman5@gmail.com>
2019-02-22 08:43:44 +0530

    Refactoring existing code

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,31 +1,46 @@
 (ns twenty48.core
   (:gen-class))
 
-(def split-identical
-  (
-    comp (partial partition-by identity) (partial remove zero?)
-  )
-)
+(def identical-partition (partial partition-by identity))
 
-(def split
-  (
-    comp (partial mapcat (partial partition-all 2)) split-identical
-  )
-)
+(def remove-zeros (partial remove zero?))
 
-(def add (partial map (partial reduce +)))
+(def partition-by-2 (partial partition-all 2))
+
+(def sanitize-lists (partial mapcat partition-by-2))
+
+(def add-list-items (partial reduce +))
+
+(def split-identical(comp identical-partition remove-zeros))
+
+(def split (comp sanitize-lists split-identical))
+
+(def add (partial map add-list-items))
 
 (defn append
   [n coll]
-  (take n (concat coll (repeat n 0))))
+  (concat coll (repeat (- n (count coll)) 0)))
 
 (defn prepend
-  [n coll]
-  (reverse (append n (reverse coll))))
+    [n coll]
+    (concat (repeat (- n (count coll)) 0) coll))
 
-(def move-grid-right (partial map (comp (partial prepend 4) add split)))
+(def append-four (partial append 4))
 
-(def move-grid-left (partial map (comp (partial append 4) add split)))
+(def prepend-four (partial prepend 4))
+
+(def add-row-items (comp add split))
+
+(def reverse-list-items (partial map reverse))
+
+(def add-zeros-right-to-result (comp append-four add-row-items))
+
+(def add-zeros-left-to-result (comp prepend-four add-row-items))
+
+(def move-grid-left (partial map add-zeros-right-to-result))
+
+(def move-grid-right (partial map add-zeros-left-to-result)
+
 
 (defn move-grid-down
   "Moves an entire grid down"
@@ -40,3 +55,4 @@
 ; ((partial map add) '((2 2) (2) (4) (2 2) (4 4)))
 ; (partial apply (partial mapcat split)) `((0 0 2 2) (0 2 0 4) (2 0 2 0) (0 4 4 0)))
 ; (move-grid-right `((0 0 2 2) (0 2 0 4) (2 0 2 0) (0 4 4 0)))
+; (move-grid-left `((0 0 2 2) (0 2 0 4) (2 0 2 0) (0 4 4 0)))
